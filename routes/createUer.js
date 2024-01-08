d1e764e492835d8edea8324c3db43ebb6a45b68e
Fardin Mustaque <fardinmustaque@Fardins-Mac-mini.local>
2024-01-08 23:22:15 +0530

    typo err fixed

--- routes/createUer.js
+++ routes/createUer.js
@@ -2,54 +2,56 @@ const express = require('express')
 const router = express.Router();
 const User = require('../model/userModel');
 
-router.post('/create-user',(req,res)=>{
-    console.log(req.body);
-    // res.send("hello");
-    const{name,baseSalary,password,confirmPassword,phone}=req.body
-    console.log("name ;"+name);
-    // console.log(baseSalary);
-    // console.log(password);
-    // console.log(phone);
+router.post('/create-user', (req, res) => {
+  console.log(req.body);
+  // res.send("hello");
+  const { name, baseSalary, password, confirmPassword, phone } = req.body
+  console.log("name ;" + name);
+  // console.log(baseSalary);
+  // console.log(password);
+  // console.log(phone);
 
 
-    const findByName = async (name) => { 
-        try {
-          const user = await User.findOne({ name });
-          if (!user) {
+  const findByName = async (name) => {
+    try {
+      const user = await User.findOne({ name });
+      if (!user) {
 
-            if (password==confirmPassword) {
-                const user = new User({
-                    name: name,
-                    phone:phone,
-                    password:password,
-                    dateOfJoin: new Date(),
-                    baseSalary:baseSalary
-                  })       
-                  user.save().then(() => {
-                    // console.log("user saved");
-                    res.json({ 
-                      message: "user Creayed succefully",
-                      message1: "Added a new member",
-      
-                   })
-                  })
-            }
-                  
-          } else {
-            console.log('user already exist ,please change the name');
-            res.json({ 
-                message: "user already exist",
-                message1: "please change the name",
+        if (password == confirmPassword) {
+          const user = new User({
+            name: name,
+            phone: phone,
+            password: password,
+            dateOfJoin: new Date(),
+            baseSalary: baseSalary
+          })
+          user.save().then(() => {
+            // console.log("user saved");
+            res.json({
+              status: 200,
+              message: "user Created succefully",
+              message1: "A new member is added in your stuff",
 
-             })
-          }
-          return user;
-        } catch (error) {
-          console.error('Error finding user:', error);
-          throw error;
+            })
+          })
         }
+
+      } else {
+        console.log('user already exist ,please change the name');
+        res.json({
+          status: 500,
+          message: "user already exist",
+          message1: "please change the name",
+
+        })
       }
-      findByName(name)
+      return user;
+    } catch (error) {
+      console.error('Error finding user:', error);
+      throw error;
+    }
+  }
+  findByName(name)
 
 
 
