2e1f0eed039bbfabf7cda17cd6c14bacba56a7cc
Fardin Mustaque <fardindeveloper1@gmail.com>
2024-01-28 11:59:30 +0530

    if attendance zero holiday salary and sunday salary removed

--- routes/slary.js
+++ routes/slary.js
@@ -1,37 +1,51 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../model/userModel');
-const Attendance = require('../model/attendenceMode');
-router.get('/salary', (req, res) => {
-
-});
+const express = require('express')
+const router = express.Router()
+const User = require('../model/userModel')
+const Attendance = require('../model/attendenceMode')
+router.get('/salary', (req, res) => { })
 
 router.post('/salary', async (req, res) => {
-    let { month, year, name } = req.body;
-    month = Number(month);
-    year = Number(year);
-    let attendances = {
-    }
+    let { month, year, name } = req.body
+    month = Number(month)
+    year = Number(year)
+    let attendances = {}
 
-    const sundaysCount = countSundaysInMonth(month, year);
+    const sundaysCount = countSundaysInMonth(month, year)
 
-    const salaryFind = async (name) => {
-        let user = await User.findOne({ name: name });
+    const salaryFind = async name => {
+        let user = await User.findOne({ name: name })
         if (user) {
-            const baseslary = user.baseSalary;
-            let userID = user._id;
-             let result = await getNumberOfPresentAttendances(userID, month, year);
-             let numberOfPresentAttendances = result.length
+            const baseslary = user.baseSalary
+            let userID = user._id
+            let result = await getNumberOfPresentAttendances(userID, month, year)
+            let numberOfPresentAttendances = result.length;
+            if (numberOfPresentAttendances===0) {
+                return res.status(200).json({
+                    name: name,
+                    month: month,
+                    year: year,
+                    numberOfPresentAttendances,
+                    sundaysCount,
+                    baseslary,
+                    finalSalary: 0,
+                    lateTimeCount:0,
+                    lateTimeSalary: 0
+                })
+            }
             let lateTimeCount = 0
 
-            result.map((value)=>{
-             lateTimeCount = lateTimeCount+ value.earlydepartureMinute+value.lateMinutes
-            });
-            lateTimeCount = (Math.round(lateTimeCount*100))/100
-            let daysalary = baseslary / 30;
+            result.map(value => {
+                lateTimeCount =
+                    lateTimeCount + value.earlydepartureMinute + value.lateMinutes
+            })
+            lateTimeCount = Math.round(lateTimeCount * 100) / 100
+            let daysalary = baseslary / 30
 
-            const lateTimeSalary = lateTimeCount *daysalary/(5*60);
-            const finalSalary = numberOfPresentAttendances * daysalary + sundaysCount * daysalary -lateTimeSalary;
+            const lateTimeSalary = (lateTimeCount * daysalary) / (5 * 60)
+            const finalSalary =
+                numberOfPresentAttendances * daysalary +
+                sundaysCount * daysalary -
+                lateTimeSalary
             res.status(200).json({
                 name: name,
                 month: month,
@@ -39,54 +53,50 @@ router.post('/salary', async (req, res) => {
                 numberOfPresentAttendances,
                 sundaysCount,
                 baseslary,
-                finalSalary: (Math.round(finalSalary*100))/100,
+                finalSalary: Math.round(finalSalary * 100) / 100,
                 lateTimeCount,
-                lateTimeSalary:(Math.round(lateTimeSalary*100)/100
-)
+                lateTimeSalary: Math.round(lateTimeSalary * 100) / 100
             })
-
-
         } else {
             res.status(200).json({
-                message:"user not found"
+                message: 'user not found'
             })
         }
     }
 
-    salaryFind(name);
-});
+    salaryFind(name)
+})
 
 function countSundaysInMonth(month, year) {
-
-    const date = new Date(year, month, 1);
-    let count = 0;
+    const date = new Date(year, month, 1)
+    let count = 0
 
     while (date.getMonth() === month) {
         if (date.getDay() === 0) {
-            count++;
+            count++
         }
-        date.setDate(date.getDate() + 1);
+        date.setDate(date.getDate() + 1)
     }
 
-    return count;
+    return count
 }
 
 const getNumberOfPresentAttendances = async (userID, month, year) => {
     try {
         const presentAttendances = await Attendance.find({
-            user: userID, status: "present", date: {
-                $gte: new Date(year, month, 1), 
-                $lte: new Date(year, month+1, 0),     
-            },
-        });
+            user: userID,
+            status: 'present',
+            date: {
+                $gte: new Date(year, month, 1),
+                $lte: new Date(year, month + 1, 0)
+            }
+        })
 
-
-        return presentAttendances;
+        return presentAttendances
     } catch (error) {
-        console.error("Error fetching attendances:", error);
-        throw error;
+        console.error('Error fetching attendances:', error)
+        throw error
     }
-};
-
+}
 
 module.exports = router
