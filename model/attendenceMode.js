1563715c0efe03719af95f02d829c13403ff5164
Fardin Mustaque <fardindeveloper1@gmail.com>
2024-01-30 14:56:52 +0530

    Untrack files listed in .gitignore

---
+++ model/attendenceMode.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const AttendanceSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+  name:{
+     type:String,
+     required:true
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['present', 'absent', 'late', 'inComplete'], 
+    default: 'absent',
+    required: true,
+  },
+  morningStatus: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  evengStatus: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  lateMinutes:{
+    type:Number,
+    default: 0,
+    require:true
+  },
+  earlydepartureMinute:{
+    type:Number,
+    default: 0,
+    require:true
+  }
+});
+
+// AttendanceSchema.index({ user: 1, date: 1 }, { unique: true });
+
+
+module.exports = mongoose.model('Attendance', AttendanceSchema);
---
+++ routes/rootUser.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const RootUser = require('../model/rootUerModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const secretKey = process.env.SECRETKEY
+
+
+
+
+router.get('/', (req, res) => {
+  res.send('hello');
+});
+
+
+
+
+router.post('/register', async (req, res) => {
+  try {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new RootUser({ username, password:hashedPassword });
+  await user.save();
+  res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+  res.status(500).json({ error: 'Registration failed' });
+  }
+  });
+
+  router.post('/login', async (req, res) => {
+    try {
+    const { username, password } = req.body;
+    const user = await RootUser.findOne({ username });
+    if (!user) {
+    return res.status(403).json({ error: 'Authentication failed',message:"Username or password is wrong" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+    return res.status(401).json({ error: 'Authentication failed',message:"Username or password is wrong" });
+    }
+    const token = jwt.sign({ userId: user._id }, secretKey, {
+    expiresIn: '2400h',
+    });
+    res.status(200).json({ token ,"message":"tokenSuccess"});
+    } catch (error) {
+    res.status(500).json({ error: 'Login failed',message:"Server error" });
+    }
+    });
+
+
+module.exports = router
