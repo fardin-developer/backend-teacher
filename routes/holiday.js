cc685a8f85cc2e56a49c89301c08182767dcb100
Fardin Mustaque <fardindeveloper1@gmail.com>
2024-01-30 14:00:26 +0530

    half holiday declared

--- routes/holiday.js
+++ routes/holiday.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Attendance = require('../model/attendenceMode')
 const User = require('../model/userModel');
+const moment = require('moment-timezone');
+
 
 
 router.get('/holiday', async (req, res) => {
@@ -12,8 +14,8 @@ router.get('/holiday', async (req, res) => {
     const isoString = modifiedDate.toISOString();
 
     let users = await User.find();
-    const promises = users.map((user,index) => {
-        
+    const promises = users.map((user, index) => {
+
         const newAttendance = new Attendance({
             user: user._id,
             name: user.name,
@@ -24,10 +26,10 @@ router.get('/holiday', async (req, res) => {
             lateMinutes: 0,
             earlyDepartureMinutes: 0
         });
-    
+
         return newAttendance.save();
     });
-    
+
     Promise.all(promises)
         .then(savedAttendances => {
             return res.status(200).json({
@@ -42,9 +44,54 @@ router.get('/holiday', async (req, res) => {
                 message: 'Failed to save attendance'
             });
         });
+
+
+
+});
+
+router.get('/half-holiday', async (req, res) => {
+    const todayIST = moment().tz('Asia/Kolkata').startOf('day');
+    let actualtoday = todayIST.valueOf();
+
+    const users = await User.find();
+
+    const promises = users.map(async (user) => {
+        const existingAttendance = await Attendance.findOne({
+            user: user._id,
+            date: {
+                $gte: actualtoday,
+                $lt: actualtoday + 24 * 60 * 60 * 1000
+            }
+        });
+        console.log(existingAttendance);
     
-
-
+        if (existingAttendance && existingAttendance.morningStatus === true && !existingAttendance.evengStatus) {
+            existingAttendance.evengStatus = true;
+            existingAttendance.status = 'present';
+            return existingAttendance.save(); 
+        } else {
+            return null; 
+        }
+    });
+    
+    Promise.all(promises)
+        .then(savedAttendances => {
+            // Filter out null values before returning
+            const validAttendances = savedAttendances.filter(attendance => attendance !== null);
+    
+            return res.status(200).json({
+                status: 'success',
+                message: 'half holiday attendance updated',
+                data: validAttendances
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({
+                status: 'error',
+                message: 'Failed to save attendance'
+            });
+        });
 });
 
 module.exports = router
