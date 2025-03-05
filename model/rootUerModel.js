7eff96f86b568162a9cdb37bd4a9bb430a150dfe
Fardin Mustaque <fardinmustaque@Fardins-Mac-mini.local>
2025-03-05 18:11:01 +0530

    user role upgraded

--- model/rootUerModel.js
+++ model/rootUerModel.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    role: {type: String,enum: ['TEST_USER', 'TEACHER', 'MASTER'], required : true},
 });
 module.exports = mongoose.model('RootUser', userSchema);
